8d11f4c477c5e65b58e54f342f63898000ce8ea5
Benjamin Fraud <benjamin.fraud@blablacar.com>
2021-01-19 10:18:43 +0100

    Add modulo strategy to post a different line each day

--- src/Command/PostLineCommand.php
+++ src/Command/PostLineCommand.php
@@ -5,13 +5,18 @@ namespace WSIIA\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Exception;
 
 class PostLineCommand extends Command
 {
+    const STRATEGY_RANDOM = 'random';
+    const STRATEGY_MODULO = 'modulo';
+
     /** @var string */
     private $slackURL;
 
@@ -36,20 +41,30 @@ class PostLineCommand extends Command
         $this
             ->setName('post-line')
             ->setDescription('Posts a Whose Line is it Anyway line to a Slack workflow.')
-            ->addArgument('line', InputArgument::OPTIONAL, 'The line to post. If empty, the command will randomly take one line from the collection of lines in lines.yaml')
+            ->addArgument('line', InputArgument::OPTIONAL, 'The line to post. If empty, the command will take one line from the collection of lines in lines.yaml depending on the requested strategy')
+            ->addOption('strategy', null, InputOption::VALUE_REQUIRED, 'The strategy to select the line (random, modulo)')
         ;
     }
 
     /**
      * {@inheritdoc}
      *
-     * @throws \Exception
+     * @throws Exception
      * @throws TransportExceptionInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $line = $input->getArgument('line');
-        $line = !is_null($line) ? $line : $this->getLine();
+        if (!is_null($input->getOption('strategy')) && !is_null($input->getArgument('line'))) {
+            throw new Exception("Strategy option cannot be used with a custom line");
+        }
+
+        if (!is_null($input->getOption('strategy'))) {
+            $line = $this->getLine($input->getOption('strategy'));
+        } elseif (!is_null($input->getArgument('line'))) {
+            $line = $input->getArgument('line');
+        } else {
+            $line = $this->getLine(self::STRATEGY_RANDOM);
+        }
 
         $client = HttpClient::create();
         $client->request('POST', $this->slackURL, [
@@ -65,13 +80,25 @@ class PostLineCommand extends Command
     }
 
     /**
+     * @param string $strategy
+     *
      * @return string
+     * @throws Exception
      */
-    private function getLine(): string
+    private function getLine(string $strategy): string
     {
         $path = __DIR__ . '/../../lines.yaml';
         $lines = Yaml::parseFile($path)['lines'];
 
-        return $lines[rand(0, count($lines) - 1)];
+        switch ($strategy) {
+            case self::STRATEGY_RANDOM:
+                return $lines[rand(0, count($lines) - 1)];
+            case self::STRATEGY_MODULO:
+                $day = date('z') + 1;
+
+                return $lines[$day % count($lines)];
+            default:
+                throw new Exception("Unknown strategy $strategy");
+        }
     }
 }
